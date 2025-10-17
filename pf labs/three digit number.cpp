0d09f281e36a2194e345d0ab14ff2ac5a696051b
mifrah11 <mifrah.iqbal11@gmail.com>
2025-10-17 21:37:15 +0500

    Add files via upload

---
+++ pf labs/three digit number.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{int n;
+printf("enter a number:");
+scanf("%d", &n);
+if(n>=100&& n<=999){
+
+printf("the number is three digit");}
+else{
+	printf("the number is not three digit");
+}
+return 0;
+}
