f30e029d51a3cf593199c4420e9d18adb2fbc031
mifrah11 <mifrah.iqbal11@gmail.com>
2025-10-25 12:40:44 +0500

    commit

---
+++ Lab exercise/lab 1/lab 1 escape sequence.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+    printf("Name\tAge\tGrade\nAli\t12\tA+\nSara\t13\tA");
+    return 0;
+}
---
+++ Lab exercise/lab 1/lab 1 github format specifier 2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    float marks = 92.5678;
+    printf("Marks: %.2f", marks);
+    return 0;
+}
