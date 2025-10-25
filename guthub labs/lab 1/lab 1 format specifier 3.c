cac226eac8be9de8a319bd0ef603c8ab7b98c279
mifrah11 <mifrah.iqbal11@gmail.com>
2025-10-25 12:46:17 +0500

    commit

---
+++ guthub labs/lab 1/lab 1 format specifier 3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    char grade = 'A';
+    printf("Your grade is %c", grade);
+    return 0;
+}
---
+++ guthub labs/lab 1/lab 1 github format specifier 1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    int age = 20;
+    printf("I am %d years old.", age);
+    return 0;
+}
