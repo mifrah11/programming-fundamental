d4de7a7945508aa94f0e86e37cedd65d7248f859
mifrah11 <mifrah.iqbal11@gmail.com>
2025-11-12 01:12:40 +0500

    commit

--- assignments/lab manual 2 exercises/lab manual2 q.1.c
+++ assignments/lab manual 2 exercises/lab manual2 q.1.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
-int main() {
-    for (int i = 0; i < 5; i++) {
-        printf("\a");  // \a is the beep (alert) sound
-    }
+int main()
+{
+	
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<i;j++)
+{
+	for(int k=0;k<j;k++){
+		printf("\t");
+	}
+
+	printf("*");
+}
+	printf("\n");
+}
     return 0;
 }
